325adb904348624815a9104faff67f2a70b6b5c6
A1093351 <107664871+A1093351@users.noreply.github.com>
2022-06-17 12:57:51 +0800

    index.php
    week1 practice

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <h1>week1 hw</h1>
+   </head>
+  <body>
+  <?php
+    $i=10;
+    $j=20;
+    echo "Hi <B> Class PHP</B>!</br>";
+    echo "i=10 j=2</br>";
+    echo "i+j=".$i+$j."<br/>";
+    echo date("Y/m/d");//顯示年月日
+    ?>
+  </body>
+</html>
